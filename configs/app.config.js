27a43500bb57433de46d0f2d70ed447595240374
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2024-12-01 12:42:50 +0600

    refactor: meta data

---
+++ configs/app.config.js
@@ -0,0 +1,11 @@
+const appConfig = {
+    title: 'Build and Manage Your Remote Team',
+    name: 'Remote Office',
+    description: `Remote Office is an end-to-end remoteOps platform that helps you build and manage global remote teams. We provide you with access to top-vetted talents, handle compliance and HR issues, and offer strategic consultation and support to optimize your team's performance and productivity. Whether you want to supercharge your existing team or build your dream team from scratch, we have the solution. Join us today and discover the benefits of working with a Remote Office.`,
+    author: {
+        name: 'Abid Hasan',
+        website: 'https://abidhasan.vercel.app/',
+    }
+}
+
+export default appConfig
